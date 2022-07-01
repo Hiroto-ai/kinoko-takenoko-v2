ad8554f65675cb9faa458b10f5c11047c97cc24f
Hiroto-ai <107395625+Hiroto-ai@users.noreply.github.com>
2022-07-01 15:11:00 +0900

    power up

--- app.py
+++ app.py
@@ -21,21 +21,32 @@ def answer():
     messages.append(request.form.get('message'))
     if len(messages) > 3:
         messages.pop(0)
+     
+ 
+
+
 
     # vote.htmlに渡す内容を変数message_htmlとしてつくる
-    message_html = ''
+    message_html = ' '
     for i in range(len(messages)):
         c = 'alert-warning ms-5' if i % 2 == 0 else 'alert-success me-5'
         m = messages[i]
+
+        # HTMLインジェクション対策
+        import re
+        m = re.sub(r'&', r'&amp;', m)
+        m = re.sub(r'<', r'&lt;', m)
+        m = re.sub(r'>', r'&gt;', m)
+        # メッセージのフォーマット
+        m = re.sub(r'\*(.+)\*', r'<strong>\1</strong>', m)
+        m = re.sub(r'(\d{2,3})-\d+-\d+', r'\1-****-****', m)
         message_html += f'<div class="alert {c}" role="alert">{m}</div>\n'
-
-
-
+	
 
     kinoko_percent = kinoko_count / (kinoko_count + takenoko_count) * 100
     takenoko_percent = takenoko_count / (kinoko_count + takenoko_count) * 100
 
     return render_template('vote.html', **vars())
-
 if __name__ == '__main__':
     app.run(debug=True)
+    
